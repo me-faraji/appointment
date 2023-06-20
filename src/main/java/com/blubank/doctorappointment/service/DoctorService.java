568286292464c2c5d888f864019b1ddd1b65f717
me-faraji <me.faraji67@gmail.com>
2023-06-20 13:06:34 -0700

    add dependency DoctorService, PatientService

--- src/main/java/com/blubank/doctorappointment/service/DoctorService.java
+++ src/main/java/com/blubank/doctorappointment/service/DoctorService.java
@@ -1,7 +1,15 @@
 package com.blubank.doctorappointment.service;
 
+import com.blubank.doctorappointment.repository.DetailCourseRepository;
+import com.blubank.doctorappointment.repository.MasterCourseRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class DoctorService {
+    private final MasterCourseRepository masterCourseRepository;
+    private final DetailCourseRepository detailCourseRepository;
+
+
 }
--- src/main/java/com/blubank/doctorappointment/service/PatientService.java
+++ src/main/java/com/blubank/doctorappointment/service/PatientService.java
@@ -1,7 +1,16 @@
 package com.blubank.doctorappointment.service;
 
+import com.blubank.doctorappointment.repository.DetailCourseRepository;
+import com.blubank.doctorappointment.repository.MasterCourseRepository;
+import com.blubank.doctorappointment.repository.PatientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PatientService {
+    private final MasterCourseRepository masterCourseRepository;
+    private final DetailCourseRepository detailCourseRepository;
+    private final PatientRepository patientRepository;
+    
 }
