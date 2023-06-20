6dfd51c19cf6fb1e7142cb92e6e3bfb4863747d2
me-faraji <me.faraji67@gmail.com>
2023-06-20 02:13:14 -0700

    add util

---
+++ src/main/java/com/blubank/doctorappointment/util/JsonUtil.java
@@ -0,0 +1,29 @@
+package com.blubank.doctorappointment.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.Set;
+
+public class JsonUtil {
+    private static final ObjectMapper OBJECT_MAPPER;
+    static {
+        OBJECT_MAPPER = new ObjectMapper();
+//        OBJECT_MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm a z"));
+        OBJECT_MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+        OBJECT_MAPPER.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
+    }
+    public static String writeValueAsString(Object val) throws Exception {
+        return OBJECT_MAPPER.writeValueAsString(val);
+    }
+    public static <T> T readValue(String val, Class<T> clazz) throws Exception {
+        return OBJECT_MAPPER.readValue(val, clazz);
+    }
+    public static <T> Set<T> readValueToList(String val, Class<T> clazz) throws Exception {
+        return OBJECT_MAPPER.readValue(val, OBJECT_MAPPER.getTypeFactory().constructCollectionType(Set.class, clazz));
+    }
+}
